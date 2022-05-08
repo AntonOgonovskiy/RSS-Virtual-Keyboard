bf8793f15f109146aedad4744c3b1fa2ff1cf312
Anton <antonogonovskiy@gmail.com>
2022-05-08 14:09:20 +0300

    fix: fix ESlint errors

--- src/body.js
+++ src/body.js
@@ -1,40 +1,45 @@
-import { row0, row1, row2, row3, row4 } from "./buttons.js";
-import { rows } from "./row.js"
-const bod = document.querySelector('body')
-export function createBody() {
-	const wrap = document.createElement('div');
-	const inp = document.createElement('textarea');
-	wrap.classList.add('wrapper');
-	inp.classList.add('input-style');
-	inp.setAttribute('id', 'input');
-	inp.setAttribute('cols', 100);
-	inp.setAttribute('rows', 10);
-	inp.setAttribute('name', 'textinput');
-	const keyboard = document.createElement('div');
-	keyboard.classList.add('keyboard');
-	for (let i = 0; i < 5; i++) {
-		const row = document.createElement('div');
-		row.classList.add('board');
-		switch (i) {
-			case 0:
-				rows.row0(row0, row);
-				break;
-			case 1:
-				rows.row1(row1, row);
-				break;
-			case 2:
-				rows.row2(row2, row);
-				break;
-			case 3:
-				rows.row3(row3, row);
-				break;
-			case 4:
-				rows.row4(row4, row);
-				break;
-		}
-		keyboard.append(row)
-	}
-	wrap.append(inp);
-	wrap.append(keyboard)
-	bod.prepend(wrap);
-};
+import {
+  row0, row1, row2, row3, row4,
+} from './buttons.js';
+import Row from './row.js';
+
+const bod = document.querySelector('body');
+export default function createBody() {
+  const wrap = document.createElement('div');
+  const inp = document.createElement('textarea');
+  wrap.classList.add('wrapper');
+  inp.classList.add('input-style');
+  inp.setAttribute('id', 'input');
+  inp.setAttribute('cols', 100);
+  inp.setAttribute('rows', 10);
+  inp.setAttribute('name', 'textinput');
+  const keyboard = document.createElement('div');
+  keyboard.classList.add('keyboard');
+  for (let i = 0; i < 5; i += 1) {
+    const row = document.createElement('div');
+    row.classList.add('board');
+    switch (i) {
+      case 0:
+        Row.row0(row0, row);
+        break;
+      case 1:
+        Row.row1(row1, row);
+        break;
+      case 2:
+        Row.row2(row2, row);
+        break;
+      case 3:
+        Row.row3(row3, row);
+        break;
+      case 4:
+        Row.row4(row4, row);
+        break;
+      default:
+        break;
+    }
+    keyboard.append(row);
+  }
+  wrap.append(inp);
+  wrap.append(keyboard);
+  bod.prepend(wrap);
+}
--- src/row.js
+++ src/row.js
@@ -1,283 +1,285 @@
 class Row {
+  static row0(row0, row) {
+    row0.forEach((i) => {
+      const key = document.createElement('div');
+      key.setAttribute('id', i.id);
+      key.classList.add('key');
+      key.classList.add(i.class);
+      const langRu = document.createElement('span');
+      const langEn = document.createElement('span');
+      const capEn = document.createElement('span');
+      const capEnUp = document.createElement('span');
+      const capRu = document.createElement('span');
+      const capRuUp = document.createElement('span');
+      const capsEn = document.createElement('span');
+      const capsEnUp = document.createElement('span');
+      const capsRu = document.createElement('span');
+      const capsRuUp = document.createElement('span');
+      langRu.classList.add('hidden');
+      langRu.classList.add('langRu');
+      langEn.classList.add('langEn');
+      capEn.classList.add('en');
+      capEnUp.classList.add('hidden');
+      capEnUp.classList.add('enUp');
+      capRu.classList.add('ru');
+      capRuUp.classList.add('hidden');
+      capRuUp.classList.add('ruUp');
+      capsEn.classList.add('hidden');
+      capsEn.classList.add('enCap');
+      capsEnUp.classList.add('hidden');
+      capsEnUp.classList.add('enCapS');
+      capsRu.classList.add('hidden');
+      capsRu.classList.add('ruCap');
+      capsRuUp.classList.add('hidden');
+      capsRuUp.classList.add('ruCapS');
+      capEn.innerHTML = i.eng;
+      capEnUp.innerHTML = i.engUp;
+      capRu.innerHTML = i.rus;
+      capRuUp.innerHTML = i.rusUp;
+      capsEn.innerHTML = i.capEn;
+      capsEnUp.innerHTML = i.capEnS;
+      capsRu.innerHTML = i.capRus;
+      capsRuUp.innerHTML = i.capRusS;
+      langRu.append(capRu);
+      langRu.append(capRuUp);
+      langRu.append(capsRu);
+      langRu.append(capsRuUp);
+      langEn.append(capsEn);
+      langEn.append(capsEnUp);
+      langEn.append(capEn);
+      langEn.append(capEnUp);
+      key.append(langEn);
+      key.append(langRu);
+      row.append(key);
+    });
+  }
 
-	row0(row0, row) {
-		for (let i of row0) {
-			const key = document.createElement('div');
-			key.setAttribute('id', i.id)
-			key.classList.add('key');
-			key.classList.add(i.class)
-			let langRu = document.createElement('span');
-			let langEn = document.createElement('span');
-			let capEn = document.createElement('span');
-			let capEnUp = document.createElement('span');
-			let capRu = document.createElement('span');
-			let capRuUp = document.createElement('span');
-			let capsEn = document.createElement('span');
-			let capsEnUp = document.createElement('span');
-			let capsRu = document.createElement('span');
-			let capsRuUp = document.createElement('span');
-			langRu.classList.add('hidden');
-			langRu.classList.add('langRu');
-			langEn.classList.add('langEn');
-			capEn.classList.add('en');
-			capEnUp.classList.add('hidden');
-			capEnUp.classList.add('enUp');
-			capRu.classList.add('ru');
-			capRuUp.classList.add('hidden');
-			capRuUp.classList.add('ruUp');
-			capsEn.classList.add('hidden');
-			capsEn.classList.add('enCap');
-			capsEnUp.classList.add('hidden');
-			capsEnUp.classList.add('enCapS');
-			capsRu.classList.add('hidden');
-			capsRu.classList.add('ruCap');
-			capsRuUp.classList.add('hidden');
-			capsRuUp.classList.add('ruCapS');
-			capEn.innerHTML = i.eng;
-			capEnUp.innerHTML = i.engUp;
-			capRu.innerHTML = i.rus;
-			capRuUp.innerHTML = i.rusUp
-			capsEn.innerHTML = i.capEn;
-			capsEnUp.innerHTML = i.capEnS;
-			capsRu.innerHTML = i.capRus;
-			capsRuUp.innerHTML = i.capRusS;
-			langRu.append(capRu);
-			langRu.append(capRuUp);
-			langRu.append(capsRu);
-			langRu.append(capsRuUp);
-			langEn.append(capsEn);
-			langEn.append(capsEnUp);
-			langEn.append(capEn);
-			langEn.append(capEnUp);
-			key.append(langEn);
-			key.append(langRu);
-			row.append(key);
-		}
-	};
-	row1(row1, row) {
-		for (let i of row1) {
-			const key = document.createElement('div');
-			key.setAttribute('id', i.id)
-			key.classList.add('key');
-			key.classList.add(i.class)
-			let langRu = document.createElement('span');
-			let langEn = document.createElement('span');
-			let capEn = document.createElement('span');
-			let capEnUp = document.createElement('span');
-			let capRu = document.createElement('span');
-			let capRuUp = document.createElement('span');
-			let capsEn = document.createElement('span');
-			let capsEnUp = document.createElement('span');
-			let capsRu = document.createElement('span');
-			let capsRuUp = document.createElement('span');
-			langRu.classList.add('hidden');
-			langRu.classList.add('langRu');
-			langEn.classList.add('langEn');
-			capEn.classList.add('en');
-			capEnUp.classList.add('hidden');
-			capEnUp.classList.add('enUp');
-			capRu.classList.add('ru');
-			capRuUp.classList.add('hidden');
-			capRuUp.classList.add('ruUp');
-			capsEn.classList.add('hidden');
-			capsEn.classList.add('enCap');
-			capsEnUp.classList.add('hidden');
-			capsEnUp.classList.add('enCapS');
-			capsRu.classList.add('hidden');
-			capsRu.classList.add('ruCap');
-			capsRuUp.classList.add('hidden');
-			capsRuUp.classList.add('ruCapS');
-			capEn.innerHTML = i.eng;
-			capEnUp.innerHTML = i.engUp;
-			capRu.innerHTML = i.rus;
-			capRuUp.innerHTML = i.rusUp;
-			capsEn.innerHTML = i.capEn;
-			capsEnUp.innerHTML = i.capEnS;
-			capsRu.innerHTML = i.capRus;
-			capsRuUp.innerHTML = i.capRusS;
-			langRu.append(capRu);
-			langRu.append(capRuUp);
-			langRu.append(capsRu);
-			langRu.append(capsRuUp);
-			langEn.append(capsEn);
-			langEn.append(capsEnUp);
-			langEn.append(capEn);
-			langEn.append(capEnUp);
-			key.append(langEn);
-			key.append(langRu);
-			row.append(key);
-		}
-	};
-	row2(row2, row) {
-		for (let i of row2) {
-			const key = document.createElement('div');
-			key.setAttribute('id', i.id)
-			key.classList.add('key');
-			key.classList.add(i.class)
-			let langRu = document.createElement('span');
-			let langEn = document.createElement('span');
-			langRu.classList.add('langRu');
-			langEn.classList.add('langEn');
-			let capEn = document.createElement('span');
-			let capEnUp = document.createElement('span');
-			let capRu = document.createElement('span');
-			let capRuUp = document.createElement('span');
-			let capsEn = document.createElement('span');
-			let capsEnUp = document.createElement('span');
-			let capsRu = document.createElement('span');
-			let capsRuUp = document.createElement('span');
-			langRu.classList.add('hidden');
-			capEn.classList.add('en');
-			capEnUp.classList.add('hidden');
-			capEnUp.classList.add('enUp');
-			capRu.classList.add('ru');
-			capRuUp.classList.add('hidden');
-			capRuUp.classList.add('ruUp');
-			capsEn.classList.add('hidden');
-			capsEn.classList.add('enCap');
-			capsEnUp.classList.add('hidden');
-			capsEnUp.classList.add('enCapS');
-			capsRu.classList.add('hidden');
-			capsRu.classList.add('ruCap');
-			capsRuUp.classList.add('hidden');
-			capsRuUp.classList.add('ruCapS');
-			capEn.innerHTML = i.eng;
-			capEnUp.innerHTML = i.engUp;
-			capRu.innerHTML = i.rus;
-			capRuUp.innerHTML = i.rusUp;
-			capsEn.innerHTML = i.capEn;
-			capsEnUp.innerHTML = i.capEnS;
-			capsRu.innerHTML = i.capRus;
-			capsRuUp.innerHTML = i.capRusS;
-			langRu.append(capRu);
-			langRu.append(capRuUp);
-			langRu.append(capsRu);
-			langRu.append(capsRuUp);
-			langEn.append(capsEn);
-			langEn.append(capsEnUp);
-			langEn.append(capEn);
-			langEn.append(capEnUp);
-			key.append(langEn);
-			key.append(langRu);
-			row.append(key);
-		}
-	};
-	row3(row3, row) {
-		for (let i of row3) {
-			const key = document.createElement('div');
-			key.setAttribute('id', i.id)
-			key.classList.add('key');
-			key.classList.add(i.class)
-			let langRu = document.createElement('span');
-			let langEn = document.createElement('span');
-			let capEn = document.createElement('span');
-			let capEnUp = document.createElement('span');
-			let capRu = document.createElement('span');
-			let capRuUp = document.createElement('span');
-			let capsEn = document.createElement('span');
-			let capsEnUp = document.createElement('span');
-			let capsRu = document.createElement('span');
-			let capsRuUp = document.createElement('span');
-			langRu.classList.add('hidden');
-			langRu.classList.add('langRu');
-			langEn.classList.add('langEn');
-			capEn.classList.add('en');
-			capEnUp.classList.add('hidden');
-			capEnUp.classList.add('enUp');
-			capRu.classList.add('ru');
-			capRuUp.classList.add('hidden');
-			capRuUp.classList.add('ruUp');
-			capsEn.classList.add('hidden');
-			capsEn.classList.add('enCap');
-			capsEnUp.classList.add('hidden');
-			capsEnUp.classList.add('enCapS');
-			capsRu.classList.add('hidden');
-			capsRu.classList.add('ruCap');
-			capsRuUp.classList.add('hidden');
-			capsRuUp.classList.add('ruCapS');
-			capEn.innerHTML = i.eng;
-			capEnUp.innerHTML = i.engUp;
-			capRu.innerHTML = i.rus;
-			capRuUp.innerHTML = i.rusUp;
-			capsEn.innerHTML = i.capEn;
-			capsEnUp.innerHTML = i.capEnS;
-			capsRu.innerHTML = i.capRus;
-			capsRuUp.innerHTML = i.capRusS;
-			langRu.append(capRu);
-			langRu.append(capRuUp);
-			langRu.append(capsRu);
-			langRu.append(capsRuUp);
-			langEn.append(capsEn);
-			langEn.append(capsEnUp);
-			langEn.append(capEn);
-			langEn.append(capEnUp);
-			key.append(langEn);
-			key.append(langRu);
-			row.append(key);
-		}
-	};
-	row4(row4, row) {
-		const keyWrap = document.createElement('div');
-		keyWrap.classList.add('key-wrap')
-		for (let i of row4) {
-			const key = document.createElement('div');
-			key.setAttribute('id', i.id)
-			key.classList.add('key');
-			key.classList.add(i.class)
-			let langRu = document.createElement('span');
-			let langEn = document.createElement('span');
-			let capEn = document.createElement('span');
-			let capEnUp = document.createElement('span');
-			let capRu = document.createElement('span');
-			let capRuUp = document.createElement('span');
-			let capsEn = document.createElement('span');
-			let capsEnUp = document.createElement('span');
-			let capsRu = document.createElement('span');
-			let capsRuUp = document.createElement('span');
-			langRu.classList.add('hidden');
-			langRu.classList.add('langRu');
-			langEn.classList.add('langEn');
-			capEn.classList.add('en');
-			capEnUp.classList.add('hidden');
-			capEnUp.classList.add('enUp');
-			capRu.classList.add('ru');
-			capRuUp.classList.add('hidden');
-			capRuUp.classList.add('ruUp');
-			capsEn.classList.add('hidden');
-			capsEn.classList.add('enCap');
-			capsEnUp.classList.add('hidden');
-			capsEnUp.classList.add('enCapS');
-			capsRu.classList.add('hidden');
-			capsRu.classList.add('ruCap');
-			capsRuUp.classList.add('hidden');
-			capsRuUp.classList.add('ruCapS');
-			capEn.innerHTML = i.eng;
-			capEnUp.innerHTML = i.engUp;
-			capRu.innerHTML = i.rus;
-			capRuUp.innerHTML = i.rusUp;
-			capsEn.innerHTML = i.capEn;
-			capsEnUp.innerHTML = i.capEnS;
-			capsRu.innerHTML = i.capRus;
-			capsRuUp.innerHTML = i.capRusS;
-			langRu.append(capRu);
-			langRu.append(capRuUp);
-			langRu.append(capsRu);
-			langRu.append(capsRuUp);
-			langEn.append(capsEn);
-			langEn.append(capsEnUp);
-			langEn.append(capEn);
-			langEn.append(capEnUp);
-			key.append(langEn);
-			key.append(langRu);
-			if (key.id === 'ArrowUp' || key.id === 'ArrowDown') {
-				keyWrap.append(key)
-				row.append(keyWrap);
-			} else {
-				row.append(key);
-			}
-		}
-	};
+  static row1(row1, row) {
+    row1.forEach((i) => {
+      const key = document.createElement('div');
+      key.setAttribute('id', i.id);
+      key.classList.add('key');
+      key.classList.add(i.class);
+      const langRu = document.createElement('span');
+      const langEn = document.createElement('span');
+      const capEn = document.createElement('span');
+      const capEnUp = document.createElement('span');
+      const capRu = document.createElement('span');
+      const capRuUp = document.createElement('span');
+      const capsEn = document.createElement('span');
+      const capsEnUp = document.createElement('span');
+      const capsRu = document.createElement('span');
+      const capsRuUp = document.createElement('span');
+      langRu.classList.add('hidden');
+      langRu.classList.add('langRu');
+      langEn.classList.add('langEn');
+      capEn.classList.add('en');
+      capEnUp.classList.add('hidden');
+      capEnUp.classList.add('enUp');
+      capRu.classList.add('ru');
+      capRuUp.classList.add('hidden');
+      capRuUp.classList.add('ruUp');
+      capsEn.classList.add('hidden');
+      capsEn.classList.add('enCap');
+      capsEnUp.classList.add('hidden');
+      capsEnUp.classList.add('enCapS');
+      capsRu.classList.add('hidden');
+      capsRu.classList.add('ruCap');
+      capsRuUp.classList.add('hidden');
+      capsRuUp.classList.add('ruCapS');
+      capEn.innerHTML = i.eng;
+      capEnUp.innerHTML = i.engUp;
+      capRu.innerHTML = i.rus;
+      capRuUp.innerHTML = i.rusUp;
+      capsEn.innerHTML = i.capEn;
+      capsEnUp.innerHTML = i.capEnS;
+      capsRu.innerHTML = i.capRus;
+      capsRuUp.innerHTML = i.capRusS;
+      langRu.append(capRu);
+      langRu.append(capRuUp);
+      langRu.append(capsRu);
+      langRu.append(capsRuUp);
+      langEn.append(capsEn);
+      langEn.append(capsEnUp);
+      langEn.append(capEn);
+      langEn.append(capEnUp);
+      key.append(langEn);
+      key.append(langRu);
+      row.append(key);
+    });
+  }
 
+  static row2(row2, row) {
+    row2.forEach((i) => {
+      const key = document.createElement('div');
+      key.setAttribute('id', i.id);
+      key.classList.add('key');
+      key.classList.add(i.class);
+      const langRu = document.createElement('span');
+      const langEn = document.createElement('span');
+      langRu.classList.add('langRu');
+      langEn.classList.add('langEn');
+      const capEn = document.createElement('span');
+      const capEnUp = document.createElement('span');
+      const capRu = document.createElement('span');
+      const capRuUp = document.createElement('span');
+      const capsEn = document.createElement('span');
+      const capsEnUp = document.createElement('span');
+      const capsRu = document.createElement('span');
+      const capsRuUp = document.createElement('span');
+      langRu.classList.add('hidden');
+      capEn.classList.add('en');
+      capEnUp.classList.add('hidden');
+      capEnUp.classList.add('enUp');
+      capRu.classList.add('ru');
+      capRuUp.classList.add('hidden');
+      capRuUp.classList.add('ruUp');
+      capsEn.classList.add('hidden');
+      capsEn.classList.add('enCap');
+      capsEnUp.classList.add('hidden');
+      capsEnUp.classList.add('enCapS');
+      capsRu.classList.add('hidden');
+      capsRu.classList.add('ruCap');
+      capsRuUp.classList.add('hidden');
+      capsRuUp.classList.add('ruCapS');
+      capEn.innerHTML = i.eng;
+      capEnUp.innerHTML = i.engUp;
+      capRu.innerHTML = i.rus;
+      capRuUp.innerHTML = i.rusUp;
+      capsEn.innerHTML = i.capEn;
+      capsEnUp.innerHTML = i.capEnS;
+      capsRu.innerHTML = i.capRus;
+      capsRuUp.innerHTML = i.capRusS;
+      langRu.append(capRu);
+      langRu.append(capRuUp);
+      langRu.append(capsRu);
+      langRu.append(capsRuUp);
+      langEn.append(capsEn);
+      langEn.append(capsEnUp);
+      langEn.append(capEn);
+      langEn.append(capEnUp);
+      key.append(langEn);
+      key.append(langRu);
+      row.append(key);
+    });
+  }
+
+  static row3(row3, row) {
+    row3.forEach((i) => {
+      const key = document.createElement('div');
+      key.setAttribute('id', i.id);
+      key.classList.add('key');
+      key.classList.add(i.class);
+      const langRu = document.createElement('span');
+      const langEn = document.createElement('span');
+      const capEn = document.createElement('span');
+      const capEnUp = document.createElement('span');
+      const capRu = document.createElement('span');
+      const capRuUp = document.createElement('span');
+      const capsEn = document.createElement('span');
+      const capsEnUp = document.createElement('span');
+      const capsRu = document.createElement('span');
+      const capsRuUp = document.createElement('span');
+      langRu.classList.add('hidden');
+      langRu.classList.add('langRu');
+      langEn.classList.add('langEn');
+      capEn.classList.add('en');
+      capEnUp.classList.add('hidden');
+      capEnUp.classList.add('enUp');
+      capRu.classList.add('ru');
+      capRuUp.classList.add('hidden');
+      capRuUp.classList.add('ruUp');
+      capsEn.classList.add('hidden');
+      capsEn.classList.add('enCap');
+      capsEnUp.classList.add('hidden');
+      capsEnUp.classList.add('enCapS');
+      capsRu.classList.add('hidden');
+      capsRu.classList.add('ruCap');
+      capsRuUp.classList.add('hidden');
+      capsRuUp.classList.add('ruCapS');
+      capEn.innerHTML = i.eng;
+      capEnUp.innerHTML = i.engUp;
+      capRu.innerHTML = i.rus;
+      capRuUp.innerHTML = i.rusUp;
+      capsEn.innerHTML = i.capEn;
+      capsEnUp.innerHTML = i.capEnS;
+      capsRu.innerHTML = i.capRus;
+      capsRuUp.innerHTML = i.capRusS;
+      langRu.append(capRu);
+      langRu.append(capRuUp);
+      langRu.append(capsRu);
+      langRu.append(capsRuUp);
+      langEn.append(capsEn);
+      langEn.append(capsEnUp);
+      langEn.append(capEn);
+      langEn.append(capEnUp);
+      key.append(langEn);
+      key.append(langRu);
+      row.append(key);
+    });
+  }
+
+  static row4(row4, row) {
+    const keyWrap = document.createElement('div');
+    keyWrap.classList.add('key-wrap');
+    row4.forEach((i) => {
+      const key = document.createElement('div');
+      key.setAttribute('id', i.id);
+      key.classList.add('key');
+      key.classList.add(i.class);
+      const langRu = document.createElement('span');
+      const langEn = document.createElement('span');
+      const capEn = document.createElement('span');
+      const capEnUp = document.createElement('span');
+      const capRu = document.createElement('span');
+      const capRuUp = document.createElement('span');
+      const capsEn = document.createElement('span');
+      const capsEnUp = document.createElement('span');
+      const capsRu = document.createElement('span');
+      const capsRuUp = document.createElement('span');
+      langRu.classList.add('hidden');
+      langRu.classList.add('langRu');
+      langEn.classList.add('langEn');
+      capEn.classList.add('en');
+      capEnUp.classList.add('hidden');
+      capEnUp.classList.add('enUp');
+      capRu.classList.add('ru');
+      capRuUp.classList.add('hidden');
+      capRuUp.classList.add('ruUp');
+      capsEn.classList.add('hidden');
+      capsEn.classList.add('enCap');
+      capsEnUp.classList.add('hidden');
+      capsEnUp.classList.add('enCapS');
+      capsRu.classList.add('hidden');
+      capsRu.classList.add('ruCap');
+      capsRuUp.classList.add('hidden');
+      capsRuUp.classList.add('ruCapS');
+      capEn.innerHTML = i.eng;
+      capEnUp.innerHTML = i.engUp;
+      capRu.innerHTML = i.rus;
+      capRuUp.innerHTML = i.rusUp;
+      capsEn.innerHTML = i.capEn;
+      capsEnUp.innerHTML = i.capEnS;
+      capsRu.innerHTML = i.capRus;
+      capsRuUp.innerHTML = i.capRusS;
+      langRu.append(capRu);
+      langRu.append(capRuUp);
+      langRu.append(capsRu);
+      langRu.append(capsRuUp);
+      langEn.append(capsEn);
+      langEn.append(capsEnUp);
+      langEn.append(capEn);
+      langEn.append(capEnUp);
+      key.append(langEn);
+      key.append(langRu);
+      if (key.id === 'ArrowUp' || key.id === 'ArrowDown') {
+        keyWrap.append(key);
+        row.append(keyWrap);
+      } else {
+        row.append(key);
+      }
+    });
+  }
 }
-const rows = new Row()
-export { rows }
+// const rows = new Row();
+export default Row;
